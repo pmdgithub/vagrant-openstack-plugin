c7177dc98ba43e3434075186a76df57d27ce7b26
John Bellone <jbellone@bloomberg.net>
2013-11-29 16:38:27 -0500

    Refactor OpenStack actions to match AWS.
    I went through all of the actions and compared them against the AWS
    equivalients. There are a few subtle differences in the instance
    actions, but for the most part they should match up. For the OpenStack
    API the concept of 'pausing' and 'unpausing' stores the machine state in
    memory instead of on disk.
    
    The functionality in this commit should encompass the following
    Vagrant commands against the OpenStack API:
            - vagrant destroy <vm>
            - vagrant up <vm>
            - vagrant reload <vm>
            - vagrant provision <vm>
            - vagrant halt <vm>
            - vagrant suspend <vm>

--- lib/vagrant-openstack-plugin/action/message_already_paused.rb
+++ lib/vagrant-openstack-plugin/action/message_already_paused.rb
@@ -1,13 +1,13 @@
 module VagrantPlugins
   module OpenStack
     module Action
-      class IsCreated
+      class MessageAlreadyPaused
         def initialize(app, env)
           @app = app
         end
 
         def call(env)
-          env[:result] = env[:machine].state.id == :active
+          env[:ui].info(I18n.t("vagrant_openstack.already_paused"))
           @app.call(env)
         end
       end
--- lib/vagrant-openstack-plugin/action/reboot_server.rb
+++ lib/vagrant-openstack-plugin/action/reboot_server.rb
@@ -3,18 +3,20 @@ require "log4r"
 module VagrantPlugins
   module OpenStack
     module Action
-      # This starts the pause server, if there is one.
-      class UnpauseServer
+      # This reboots a running server, if there is one.
+      class RebootServer
         def initialize(app, env)
           @app    = app
-          @logger = Log4r::Logger.new("vagrant_openstack::action::unpause_server")
+          @logger = Log4r::Logger.new("vagrant_openstack::action::reboot_server")
         end
 
         def call(env)
           if env[:machine].id
-            env[:ui].info(I18n.t("vagrant_openstack.unpausing_server"))
+            env[:ui].info(I18n.t("vagrant_openstack.rebooting_server"))
+
+            # TODO: Validate the fact that we get a server back from the API.
             server = env[:openstack_compute].servers.get(env[:machine].id)
-            server.unpause
+            server.reboot('SOFT')
           end
 
           @app.call(env)
