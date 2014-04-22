ea10bff5cdabea0b9c543a5231500fb7f9fd50ac
Last G <last_g@hackerdom.ru>
2014-04-22 20:34:34 +0600

    Missing region while configuring networks

--- lib/vagrant-openstack-plugin/action/connect_openstack.rb
+++ lib/vagrant-openstack-plugin/action/connect_openstack.rb
@@ -46,6 +46,7 @@ module VagrantPlugins
               :openstack_api_key => api_key,
               :openstack_auth_url => endpoint,
               :openstack_tenant => tenant
+              :openstack_region => region
             })
           end
 
